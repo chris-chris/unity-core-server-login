b04ef24ffd57b0f5c1506b2130b92db103ec65aa
Karadress <karadress.kim@gmail.com>
2017-07-08 16:10:18 +0900

    Initialize source code

---
+++ Controllers/UpdateResultController.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DotnetCoreServer.Models;
+
+namespace DotnetCoreServer.Controllers
+{
+    [Route("[controller]/[action]")]
+    public class UpdateResultController : Controller
+    {
+        IUserDao userDao;
+        IStageResultDao stageResultDao;
+        public UpdateResultController(IUserDao userDao,IStageResultDao IStageResultDao) {
+            this.userDao = userDao;
+            this.stageResultDao = IStageResultDao;
+        }
+
+        [HttpPost]
+        public ResultBase Post([FromBody] StageRequest request) {
+            ResultBase result = new ResultBase();
+            User user = userDao.GetUser(request.UserID);
+            user.Point = user.Point + request.Point;
+            userDao.UpdateUser(user);
+            stageResultDao.InsertStageRecord(request.UserID, request.Point);
+            result.ResultCode = 1;
+            result.Message = "Success";
+            return result;
+        }
+        
+    }
+}
---
+++ Models/Dao/StageResultDao.cs
@@ -0,0 +1,43 @@
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+
+namespace DotnetCoreServer.Models
+{
+    public interface IStageResultDao{
+        bool InsertStageRecord(long UserID, int Point);
+    }
+
+    public class StageResultDao : IStageResultDao
+    {
+        public IDB db {get;}
+
+        public StageResultDao(IDB db){
+            this.db = db;
+        }
+
+        public bool InsertStageRecord(long UserID, int Point){
+            
+            string query = String.Format(
+                @"INSERT INTO tb_stage_result 
+                (user_id, point, record_time)
+                  VALUES ('{0}','{1}', now())",
+                    UserID, Point);
+
+            Console.WriteLine(query);
+
+            using(MySqlConnection conn = db.GetConnection())
+            using(MySqlCommand cmd = (MySqlCommand)conn.CreateCommand())
+            {
+
+                cmd.CommandText = query;
+                cmd.ExecuteNonQuery();
+
+                conn.Close();
+            }
+
+        
+            return true;
+        }
+    }
+}
